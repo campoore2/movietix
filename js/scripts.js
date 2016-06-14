3d819856710fc170c41cae9b527ba559042250ad
Cameron Poore and Matthew Giem <cameron+matthew@>
2016-06-14 12:01:10 -0700

    add modals to all the movie selection and javascript to git the movies a price

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,39 @@
 //frontend logic
 
 $(document).ready(function()  {
-  $("#").
-  event.preventDefault();
+  $("#jawsTicket").click(function() {
+    $("#modalTitle").text("Jaws");
+  });
+  $("#starWarsTicket").click(function() {
+    $("#modalTitle").text("Star Wars");
+  });
+  $("#independenceDayTicket").click(function() {
+    $("#modalTitle").text("Indpendence Day");
+  });
+  $("#pulpFictionTicket").click(function() {
+    $("#modalTitle").text("Pulp Fiction");
+  });
+  $("#theUsualSuspectsTicket").click(function() {
+    $("#modalTitle").text("The Usual Suspects");
+  });
+  $("#warCraftTicket").click(function() {
+    $("#modalTitle").text("War Craft");
+  });
+  $(".purchaseTicket").click(function() {
+    $('.blank')[0].reset();
+    $("#priceForTicket").text("");
+
+  })
+  $("#getPrice").click(function(){
+    var ticketType = parseInt($("input:radio[name='optradio']:checked").val());
+    var ticketTime = parseInt($("#dropdown option:selected").val());
+    debugger;
+    var ticket = new movieChoice(3,ticketTime,ticketType);
+    $("#priceForTicket").text(ticket.moviePrice());
+    console.log(ticket.moviePrice());
+    event.preventDefault();
+  });
+
 })
 
 //backend logic
